6c41f30fb10b445c5b194690faf6751e76a48ab4
andreaserrecchia <andreaserrecchia@hotmail.it>
2023-05-10 22:37:24 +0200

    feat(i3): i3 and i3blocks config, remove polybar

--- arch-setup.sh
+++ arch-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-pacman -Sy neovim alacritty i3 polybar picom zsh dunst rofi htop feh thunar xclip discord pavucontrol neofetch pipewire pipewire-pulse pipewire-media-session google-chrome reflector exa i3blocks fd ripgrep obsidian
+pacman -Sy neovim alacritty i3 i3blocks picom zsh dunst rofi htop feh thunar xclip discord pavucontrol neofetch pipewire pipewire-pulse pipewire-media-session google-chrome reflector exa fd ripgrep obsidian
 
 ln -s $HOME/dotfiles/nvim ~/.config
 ln -s $HOME/dotfiles/alacritty ~/.config
--- i3/tray.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-killall nm-applet
-killall volumeicon
-nm-applet --indicator
-volumeicon
