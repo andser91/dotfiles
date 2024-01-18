716700d10f46832aa092b7e491930fa354551f4a
Andrea Serrecchia <andrea.serrecchia@network-contacts.it>
2024-01-18 22:18:33 +0100

    feat(): bat

--- arch-setup.sh
+++ arch-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-pacman -Sy neovim alacritty i3-wm i3blocks picom zsh dunst rofi htop feh thunar xclip discord pavucontrol neofetch pipewire pipewire-pulse pipewire-media-session reflector exa fd ripgrep qt5ct kvantum tree docker docker-compose 
+pacman -Sy neovim alacritty i3-wm i3blocks picom zsh dunst rofi htop feh thunar xclip discord pavucontrol neofetch pipewire pipewire-pulse pipewire-media-session reflector exa fd ripgrep qt5ct kvantum tree docker docker-compose bat 
 
 yay -Sy google-chrome whitesur-icon-theme
 
@@ -16,5 +16,6 @@ ln -s $HOME/dotfiles/htop ~/.config
 ln -s $HOME/dotfiles/wallpapers ~/.config
 ln -s $HOME/dotfiles/lesspipe/.lesskey ~
 ln -s $HOME/dotfiles/lesspipe/.lessfilter ~
+ln -s $HOME/dotfiles/git/.gitconfig ~
 rm -rf $HOME/.config/neofetch && ln -s $HOME/dotfiles/neofetch ~/.config
 sudo ln -s $HOME/dotfiles/Fluent-Dark-compact /usr/share/themes
