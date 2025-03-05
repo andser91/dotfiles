a0d7b24c65bf1257816a2e9f44fb9ab606e202d0
Andrea Serrecchia <andrea.serrecchia@network-contacts.it>
2025-03-05 11:53:09 +0100

    feat(): nvim treesitter textobjects

--- nvim/lua/andrea/plugins/treesitter.lua
+++ nvim/lua/andrea/plugins/treesitter.lua
@@ -5,14 +5,50 @@ return {
 		build = ":TSUpdate",
 		dependencies = {
 			"windwp/nvim-ts-autotag",
+			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
 		config = function()
 			local config = require("nvim-treesitter.configs")
 			config.setup({
-				highlight = { enable = true },
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
 				indent = { enable = true },
 				autotag = { enable = true },
 				auto_install = true,
+				rainbow = {
+					enable = true,
+					extended_mode = true,
+					max_file_lines = nil,
+				},
+				ensure_installed = {
+					"json",
+					"javascript",
+					"go",
+					"bash",
+					"toml",
+					"java",
+					"rust",
+					"css",
+					"html",
+					"yaml",
+					"lua",
+					"dockerfile",
+					"gitignore",
+					"query",
+					"sql",
+					"typescript",
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
 			})
 		end,
 	},
